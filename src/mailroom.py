7a9243d8db223b47afb8dce6ba480efbf2af9026
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-12-08 15:37:41 -0800

    adding tests for list_check and add_donation functions, tests are currently passing.

--- src/mailroom.py
+++ src/mailroom.py
@@ -2,35 +2,35 @@
 
 import io
 from faker import Faker
-from builtins import input
+# from builtins import input
 
 donors = {}
 
 
-def get_input():
-    """Function gathers all user input and controls program flow."""
-    ask_report = input("Thankyou letter (1), report (2) or quit? (q)")
-    if ask_report == 1:
-        thankyou_input = input("Do you want a list (1) or to enter a donor name (2)")
-        while thankyou_input == 1:
-            show_list()
-            thankyou_input = input("Do you want a list (1) or to enter a donor name (2)")
-        if thankyou_input == 2:
-            name_added = input("Please enter the donor name:")
-            list_check(name_added)
-            donor_amount = input("How much did they donate?")
-            add_donation(name_added, donor_amount)
-            # print_email()
-    elif ask_report == 2:
+# def get_input():
+#     """Function gathers all user input and controls program flow."""
+#     ask_report = input("Thankyou letter (1), report (2) or quit? (q)")
+#     if ask_report == 1:
+#         thankyou_input = input("Do you want a list (1) or to enter a donor name (2)")
+#         while thankyou_input == 1:
+#             show_list()
+#             thankyou_input = input("Do you want a list (1) or to enter a donor name (2)")
+#         if thankyou_input == 2:
+#             name_added = input("Please enter the donor name:")
+#             list_check(name_added)
+#             donor_amount = input("How much did they donate?")
+#             add_donation(name_added, donor_amount)
+#             # print_email()
+#     elif ask_report == 2:
 
-    elif ask_report.lower() == 'q':
+#     elif ask_report.lower() == 'q':
 
-    else:
-        print("Incorrect input, please enter 1, 2 or q")
-        get_input()
+#     else:
+#         print("Incorrect input, please enter 1, 2 or q")
+#         get_input()
 
 
-def show_list():
+# def show_list():
 
 
 def list_check(name_added):
--- src/test_mailroom.py
+++ src/test_mailroom.py
@@ -2,3 +2,22 @@
 
 
 import pytest
+from faker import Faker
+
+    # donors = {
+    #                 'Matthew Wade': [50.00, 100.00],
+    #                 'Betty Stewart': [100.00, 50.00, 150.00],
+    #                 'Timothy Berg': [10.00],
+    #                 'Paul Benson': [5]}
+
+def test_list_check():
+    """Tests to see whether names are added to the donor table"""
+    from mailroom import list_check, donors
+    list_check('Patrick Stewart')
+    assert donors['Patrick Stewart'] == []
+
+
+def test_add_donation():
+    from mailroom import add_donation, donors
+    add_donation('Patrick Stewart', 50.00)
+    assert donors['Patrick Stewart'] == [50.00]
