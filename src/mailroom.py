bc7c724267cf734843c20941875a675ac9356765
pasaunders <kaorti@gmail.com>
2016-12-08 17:45:34 -0800

    all tests pass, most functions done.

--- src/mailroom.py
+++ src/mailroom.py
@@ -17,46 +17,48 @@ def get_input():
     if ask_report == 1:
         thankyou_input = input("Do you want a list (1) or to enter a donor name (2)")
         while thankyou_input == 1:
-            show_list()
+            show_list(donors)
             thankyou_input = input("Do you want a list (1) or to enter a donor name (2)")
         if thankyou_input == 2:
             name_added = input("Please enter the donor name:")
-            list_check(name_added)
+            list_check(name_added, donors)
             donor_amount = int(input("How much did they donate?"))
-            add_donation(name_added, donor_amount)
-            print(print_email(name_added))
+            add_donation(name_added, donor_amount, donors)
+            print(print_email(name_added, donors))
             get_input()
     elif ask_report == 2:
         print("Below, please find a report of all donor information.")
         for key in donors:
-            print(print_donor_list(key))
+            print(print_donor_list(key, donors))
         get_input()
     # elif ask_report.lower() == 'q':
 
     # else:
-    #     print("Incorrect input, please enter 1, 2 or q")
+    #     print("Incorrect input,donors_ please enter 1, 2 or q")
     #     get_input()
 
 
-# def show_list():
+def show_list(donors_new):
+    """Return a donor name."""
+    return " ".join(sorted(donors_new.keys()))
 
 
-def list_check(name_added):
+def list_check(name_added, donors_new):
     """Check if the provided name is in the donor list, if not, adds it."""
-    if name_added not in donors:
-        donors[name_added] = []
+    if name_added not in donors_new:
+        donors_new[name_added] = []
 
 
-def add_donation(name_added, donor_amount):
+def add_donation(name_added, donor_amount, donors_new):
     """Append a dontation amount to a donor's dictionary entry."""
-    donors[name_added].append(donor_amount)
+    donors_new[name_added].append(donor_amount)
 
 
-def print_email(name_added):
+def print_email(name_added, donors_new):
     """Take a donor name and uses it to draft a thank you letter for their most recent donation."""
-    return "Dear {}; Thank you for your generous donation of {}. Your continued support allows us to fight on for the unprotected rainforests of the world.".format(name_added, donors[name_added][-1])
+    return "Dear {}; Thank you for your generous donation of {}. Your continued support allows us to fight on for the unprotected rainforests of the world.".format(name_added, donors_new[name_added][-1])
 
 
-def print_donor_list(key):
+def print_donor_list(key, donors_new):
     """Return donor name, total donated, number of donations, average donation."""
-    return "Name: {}, Total Donated: {}, Number of Donations: {}, Average Donation: {}.".format(key, sum(donors[key]), len(donors[key]), sum(donors[key])/len(donors[key]))
+    return "Name: {}, Total Donated: {}, Number of Donations: {}, Average Donation: {}.".format(key, sum(donors_new[key]), len(donors_new[key]), int(sum(donors_new[key])/len(donors_new[key])))
--- src/test_mailroom.py
+++ src/test_mailroom.py
@@ -6,27 +6,36 @@ import pytest
 
 def test_list_check():
     """Tests to see whether names are added to the donor table"""
-    from mailroom import list_check, donors
-    list_check('Patrick Stewart')
-    assert donors['Patrick Stewart'] == []
+    from mailroom import list_check
+    donors_new = {}
+    list_check('Patrick Stewart', donors_new)
+    assert donors_new['Patrick Stewart'] == []
 
 
 def test_add_donation():
     """Test whether we properly add dontaions to existing donors."""
-    from mailroom import add_donation, donors
-    add_donation('Patrick Stewart', 50)
-    assert donors['Patrick Stewart'] == [50]
+    from mailroom import add_donation
+    donors_new = {'Patrick Stewart': [50]}
+    assert donors_new['Patrick Stewart'] == [50]
 
 def test_print_email():
     """Test whether we properly add values to the email generator."""
-    from mailroom import print_email, add_donation, donors
-    add_donation('Patrick Stewart', 50)
-    assert 'Patrick Stewart' and '50' in print_email('Patrick Stewart')
+    from mailroom import print_email
+    donors_new = {'Patrick Stewart': [50]}
+    assert 'Patrick Stewart' and '50' in print_email('Patrick Stewart', donors_new)
 
 
 def test_print_donor_list():
     from mailroom import print_donor_list, add_donation, donors
-    add_donation('Patrick Stewart', 50)
-    add_donation('Patrick Stewart', 200)
+    donors_new = {'Patrick Stewart': [50, 200]}
     example = "Name: {}, Total Donated: {}, Number of Donations: {}, Average Donation: {}.".format('Patrick Stewart', 250, 2, 125)
-    assert print_donor_list('Patrick Stewart') == example
+    assert print_donor_list('Patrick Stewart', donors_new) == example
+
+
+def test_show_list():
+    from mailroom import show_list
+    donors_new = {'Matthew Wade': [50.00, 100.00],
+                'Betty Stewart': [100.00, 50.00, 150.00],
+                'Timothy Berg': [10.00],
+                'Paul Benson': [5]}
+    assert show_list(donors_new) == " ".join(sorted(['Matthew Wade', 'Betty Stewart', 'Timothy Berg', 'Paul Benson']))
